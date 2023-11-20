7f41dd632f99853b81bf90e8aef5fe0ce4cbced3
Mbayrak25 <mbayrak25@outlook.com>
2023-11-20 21:21:49 +0300

    asd

--- dartapp/bin/class.dart
+++ dartapp/bin/class.dart
@@ -1,59 +1,8 @@
-void main() {
-  var personelManager = PersonelManager();
-  personelManager.add();
-  var customerManager = CustomerManager();
-  var customer = Customer.withInfo("Ali", "Veli");
-  var customer2 = Customer2();
-  //customer.firstName = "Tunçi";
-  //customer.lastName = "Kanki";
-  customer2.adi = "Kemal";
-  customer2.soyadi = "Can";
-  customerManager.add(customer);
-  customerManager.add2(customer2);
+void main() {}
+void selamVer() {
+  print("Selam");
 }
 
-//////////////////////////////////
-class Customer {
-  //String firstName, lastName = "";
-  late String firstName, lastName;
-  Customer.withInfo(this.firstName, this.lastName);
-}
-
-class Customer2 {
-  late String adi, soyadi;
-}
-
-////////////////////////////////
-class CustomerManager {
-  /////
-  void add(Customer customer) {
-    print("Added! ${customer.firstName} ${customer.lastName}");
-  }
-
-  ////
-  void add2(Customer2 customer2) {
-    print("Added! ${customer2.adi} ${customer2.soyadi}");
-  }
-
-  void update() {
-    print("Updated!");
-  }
-
-  void delete() {
-    print("Deleted!");
-  }
-}
-
-class PersonelManager {
-  void add() {
-    print("Added");
-  }
-
-  void update() {
-    print("Updated!");
-  }
-
-  void delete() {
-    print("Deleted!");
-  }
+void selamVer2(String userName) {
+  print("Selam $userName");
 }
--- dartapp/bin/functions.dart
+++ dartapp/bin/functions.dart
@@ -1,7 +1,26 @@
 void main() {
   selamVer2("Ahmet");
+  selamVer();
+  double hesapSonucu = hesapla(15341, 321);
+  print(hesapSonucu);
+  test1(1, 2, 3);
 }
 
 void selamVer2(String kullanici) {
   print("Selam $kullanici");
+}
+
+void selamVer() {
+  print("Kanka");
+}
+
+double hesapla(double krediTutari, double yuzde) {
+  double sonuc = krediTutari * yuzde / 100;
+  return (sonuc);
+}
+
+void test1(int sayi1, int sayi2, int sayi3) {
+  print(sayi1);
+  print(sayi2);
+  print(sayi3);
 }
